f3b7fd49d8a8310e1e9dd41429c236f635c8e526
filipkrunic <fkrunic9@gmail.com>
2023-01-30 09:34:24 -0800

    Day 2 solution

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main (main) where
 
+import qualified Data.Text as T
+
 import qualified Day1 as D01
+import qualified Day2 as D02
 
 day1Main :: IO ()
 day1Main = do 
@@ -8,5 +11,11 @@ day1Main = do
   print (D01.part1Solution input)
   print (D01.part2Solution input)
 
+day2Main :: IO () 
+day2Main = do
+  input <- T.pack <$> readFile "inputs/day2.txt"
+  print (D02.part1Solution input)
+  print (D02.part2Solution input)
+
 main :: IO ()
-main = day1Main
+main = day2Main
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 import Test.Hspec
 
 import qualified Day1 as D01
+import qualified Day2 as D02
 
 main :: IO ()
 main = hspec $ do
@@ -8,4 +9,8 @@ main = hspec $ do
     it "Day 1 Solution - Part 1" $ do
       D01.part1Solution D01.puzzleInput `shouldBe` 24000
     it "Day 1 Solution - Part 2" $ do
-      D01.part2Solution D01.puzzleInput `shouldBe` 45000      
+      D01.part2Solution D01.puzzleInput `shouldBe` 45000    
+    it "Day 2 Solution - Part 1" $ do 
+      D02.part1Solution D02.puzzleInput `shouldBe` 15
+    it "Day 2 Solution - Part 2" $ do 
+      D02.part2Solution D02.puzzleInput `shouldBe` 12     
