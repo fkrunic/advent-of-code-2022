f49743d717ba611dd7ff295e5d80934cde3792e9
filipkrunic <fkrunic9@gmail.com>
2023-02-02 17:59:26 -0800

    parsing monkey data

--- src/Day11.hs
+++ src/Day11.hs
@@ -12,16 +12,17 @@ import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
-
-data MonkeyOp = Add Int | Multiply Int deriving (Show, Eq)
+data MonkeyOp = Add (Maybe Int) | Multiply (Maybe Int) deriving (Show, Eq)
 type ThrowChoice = (Int, Int)
 data Monkey
   = Monkey 
   { label :: Int
   , startingItems :: [Int]
   , operation :: MonkeyOp
+  , divisibility :: Int
   , throwChoice :: (Int, Int)
   }
+  deriving (Show, Eq)
 
 -------------------------------------------------------------------------------------
 
@@ -41,15 +42,24 @@ signedInteger :: Parser Int
 signedInteger = L.signed sc integer
 
 pMonkeyHeader :: Parser Int
-pMonkeyHeader = symbol "Monkey" *> integer
+pMonkeyHeader = symbol "Monkey" *> integer <* symbol ":"
 
 pStartingItems :: Parser [Int]
-pStartingItems = some (integer <* optional (symbol ","))
+pStartingItems = 
+  symbol "Starting items:" *>
+  some (integer <* optional (symbol ","))
+
+pOpReference :: Parser (Maybe Int)
+pOpReference = 
+  choice 
+  [ symbol "old" $> Nothing
+  , Just <$> integer
+  ]
 
 pMonkeyOp :: Parser MonkeyOp
 pMonkeyOp = choice
-  [ Add <$> (symbol "+" *> integer)
-  , Multiply <$> (symbol "*" *> integer)
+  [ Add <$> (symbol "+" *> pOpReference)
+  , Multiply <$> (symbol "*" *> pOpReference)
   ]
 
 pOperation :: Parser MonkeyOp
@@ -66,7 +76,43 @@ pThrowChoice =
 
 pMonkey :: Parser Monkey
 pMonkey = 
-
+  Monkey <$> 
+    pMonkeyHeader <*>
+    (space *> pStartingItems) <*>
+    (space *> pOperation) <*>
+    (space *> pDivisibility) <*>
+    (space *> pThrowChoice)
 
 -------------------------------------------------------------------------------------
 
+puzzleInput :: Text
+puzzleInput =
+  T.intercalate "\n"
+  [ "Monkey 0:"
+  , "  Starting items: 79, 98"
+  , "  Operation: new = old * 19"
+  , "  Test: divisible by 23"
+  , "    If true: throw to monkey 2"
+  , "    If false: throw to monkey 3"
+  , ""
+  , "Monkey 1:"
+  , "  Starting items: 54, 65, 75, 74"
+  , "  Operation: new = old + 6"
+  , "  Test: divisible by 19"
+  , "    If true: throw to monkey 2"
+  , "    If false: throw to monkey 0"
+  , ""
+  , "Monkey 2:"
+  , "  Starting items: 79, 60, 97"
+  , "  Operation: new = old * old"
+  , "  Test: divisible by 13"
+  , "    If true: throw to monkey 1"
+  , "    If false: throw to monkey 3"
+  , ""
+  , "Monkey 3:"
+  , "  Starting items: 74"
+  , "  Operation: new = old + 3"
+  , "  Test: divisible by 17"
+  , "    If true: throw to monkey 0"
+  , "    If false: throw to monkey 1"
+  ]
