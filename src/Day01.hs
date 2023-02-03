d5d4e77afc1cebc9fde872b336e7c8073bf45cb8
filipkrunic <fkrunic9@gmail.com>
2023-02-03 15:57:57 -0800

    nits for day 01

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,37 +1,56 @@
-module Day01 where
+module Day01 (
+  part1Solution,
+  part2Solution,
+  puzzleInput,
+) where
 
-import Data.List (sortOn, intercalate)
+import Data.List (intercalate, sortOn)
 
 calorieChunker :: [String] -> [Int]
 calorieChunker [] = []
-calorieChunker cls = 
+calorieChunker cls =
   if null chunk
     then calorieChunker (tail rest)
     else total : calorieChunker rest
-  where
-    (chunk, rest) = span (/= "") cls
-    total = (sum . map read) chunk
+ where
+  (chunk, rest) = span (/= "") cls
+  total = (sum . map read) chunk
 
-part1Solution :: String -> Int 
-part1Solution = snd . head . sortOn (negate . snd) . zip [1..] . calorieChunker . lines
+part1Solution :: String -> Int
+part1Solution =
+  snd
+    . head
+    . sortOn (negate . snd)
+    . zip [1 :: Int ..]
+    . calorieChunker
+    . lines
 
-part2Solution :: String -> Int 
-part2Solution = sum . map snd . take 3 . sortOn (negate . snd) . zip [1..] . calorieChunker . lines
+part2Solution :: String -> Int
+part2Solution =
+  sum
+    . map snd
+    . take 3
+    . sortOn (negate . snd)
+    . zip [1 :: Int ..]
+    . calorieChunker
+    . lines
 
 puzzleInput :: String
-puzzleInput = intercalate "\n"
-  [ "1000"
-  , "2000"
-  , "3000"
-  , ""
-  , "4000"
-  , ""
-  , "5000"
-  , "6000"
-  , ""
-  , "7000"
-  , "8000"
-  , "9000"
-  , ""
-  , "10000"
-  ]
+puzzleInput =
+  intercalate
+    "\n"
+    [ "1000"
+    , "2000"
+    , "3000"
+    , ""
+    , "4000"
+    , ""
+    , "5000"
+    , "6000"
+    , ""
+    , "7000"
+    , "8000"
+    , "9000"
+    , ""
+    , "10000"
+    ]
