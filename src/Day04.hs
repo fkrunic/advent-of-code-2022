377ca25d989fc7b8b0c3d979b7b28112a41c27dd
filipkrunic <fkrunic9@gmail.com>
2023-02-03 16:07:13 -0800

    nits for day 04

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Day04 where
+module Day04 (
+  part1Solution,
+  part2Solution,
+  puzzleInput,
+) where
 
 import Data.Either (fromRight)
+import qualified Data.Set as S
 import Data.Text (Text)
 import qualified Data.Text as T
-import qualified Data.Set as S
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer as L
+import Prelude hiding ((<=), (^))
 
 type Parser = Parsec Void Text
 type Assignment = (Int, Int)
@@ -24,44 +27,44 @@ integer :: Parser Int
 integer = lexer L.decimal
 
 pAssignment :: Parser Assignment
-pAssignment = 
+pAssignment =
   (,) <$> (integer <* char '-') <*> integer
 
 pLine :: Parser (Assignment, Assignment)
-pLine = 
+pLine =
   (,) <$> (pAssignment <* char ',') <*> pAssignment
 
 isOverlapped :: Assignment -> Assignment -> Bool
 isOverlapped (s1, e1) (s2, e2) = (g1 <= g2) || (g2 <= g1)
-  where
-    r1 = [s1 .. e1]
-    r2 = [s2 .. e2]
-    g1 = S.fromList r1
-    g2 = S.fromList r2
-    (<=) = S.isSubsetOf
+ where
+  r1 = [s1 .. e1]
+  r2 = [s2 .. e2]
+  g1 = S.fromList r1
+  g2 = S.fromList r2
+  (<=) = S.isSubsetOf
 
 anyOverlap :: Assignment -> Assignment -> Bool
 anyOverlap (s1, e1) (s2, e2) = not $ null $ S.intersection g1 g2
-  where
-    r1 = [s1 .. e1]
-    r2 = [s2 .. e2]
-    g1 = S.fromList r1
-    g2 = S.fromList r2  
+ where
+  r1 = [s1 .. e1]
+  r2 = [s2 .. e2]
+  g1 = S.fromList r1
+  g2 = S.fromList r2
 
-part1Solution :: Text -> Int 
-part1Solution = length . filter (uncurry isOverlapped) . fromRight [] . parser 
-  where
-    parser = runParser (some pLine) ""
+part1Solution :: Text -> Int
+part1Solution = length . filter (uncurry isOverlapped) . fromRight [] . parser
+ where
+  parser = runParser (some pLine) ""
 
 part2Solution :: Text -> Int
-part2Solution = length . filter (uncurry anyOverlap) . fromRight [] . parser 
-  where
-    parser = runParser (some pLine) ""
+part2Solution = length . filter (uncurry anyOverlap) . fromRight [] . parser
+ where
+  parser = runParser (some pLine) ""
 
 puzzleInput :: Text
-puzzleInput = 
+puzzleInput =
   T.intercalate
-  "\n"
+    "\n"
     [ "2-4,6-8"
     , "2-3,4-5"
     , "5-7,7-9"
