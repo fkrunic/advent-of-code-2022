96ecf3b2b440d9ee09825929b4a6c0bb91c787f6
filipkrunic <fkrunic9@gmail.com>
2023-02-05 14:59:13 -0800

    day 5 nits

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Day05 where
+module Day05 (
+  part1Solution,
+  part2Solution,
+  puzzleInput,
+) where
 
 import Data.Either (fromRight)
 import Data.Functor (($>))
 import Data.List (sortOn, uncons)
 import Data.Map (Map)
-import Data.Maybe (fromJust)
 import qualified Data.Map.Strict as M
-import qualified Data.Set as S
+import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Void
@@ -20,19 +21,17 @@ type Parser = Parsec Void Text
 
 data Slot = Empty | Crate Char deriving (Show, Eq)
 
-data GridSlice
-  = GridSlice
+data GridSlice = GridSlice
   { gridSliceSlot :: Slot
   , gridSliceXCoord :: Int
-  } deriving (Show, Eq)
+  }
+  deriving (Show, Eq)
 
-data GridPoint
-  = GridPoint
+data GridPoint = GridPoint
   { gpSlot :: Slot
   , gpCoord :: (Int, Int)
-  } deriving (Show, Eq)
-
-type Line = [GridSlice]
+  }
+  deriving (Show, Eq)
 
 type Move = (Int, Int, Int)
 
@@ -40,7 +39,7 @@ type Input = ([GridPoint], [Move])
 
 type Depot = Map Int [Char]
 
-------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 sc :: Parser ()
 sc = L.space space1 empty empty
@@ -56,23 +55,23 @@ integer = lexer L.decimal
 
 pSlot :: Parser Slot
 pSlot = try pEmpty <|> pCrate
-  where
-    pEmpty = spaceChar *> spaceChar *> spaceChar $> Empty
-    pCrate = Crate <$> (char '[' *> letterChar <* char ']')
+ where
+  pEmpty = spaceChar *> spaceChar *> spaceChar $> Empty
+  pCrate = Crate <$> (char '[' *> letterChar <* char ']')
 
 pLine :: Parser [GridSlice]
 pLine = map buildSlice . zip [1 ..] <$> some (pSlot <* optional (char ' '))
-  where
-    buildSlice (xCoord, slot) = GridSlice slot xCoord
+ where
+  buildSlice (xCoord, slot) = GridSlice slot xCoord
 
 slicesToPoints :: Int -> [GridSlice] -> [GridPoint]
-slicesToPoints yCoord = map $ \slice -> 
-  GridPoint (gridSliceSlot slice) (gridSliceXCoord slice, yCoord) 
+slicesToPoints yCoord = map $ \slice ->
+  GridPoint (gridSliceSlot slice) (gridSliceXCoord slice, yCoord)
 
 pStack :: Parser [GridPoint]
-pStack = generatePoints . zip [1..] <$> some (pLine <* optional (char '\n'))
-  where
-    generatePoints = concatMap (uncurry slicesToPoints)
+pStack = generatePoints . zip [1 ..] <$> some (pLine <* optional (char '\n'))
+ where
+  generatePoints = concatMap (uncurry slicesToPoints)
 
 pLabels :: Parser [Int]
 pLabels = spaceChar *> some integer
@@ -90,7 +89,7 @@ pMove =
     <*> (symbol "from" *> integer)
     <*> (symbol "to" *> integer)
 
-------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 addToStack :: Int -> Slot -> Depot -> Depot
 addToStack _ Empty = id
@@ -98,9 +97,9 @@ addToStack ident (Crate c) = M.insertWith (++) ident [c]
 
 buildDepot :: [GridPoint] -> Depot
 buildDepot = foldr builder M.empty . sortOn (snd . gpCoord)
-  where
-    builder gp = addToStack (getX gp) (gpSlot gp)
-    getX = fst . gpCoord    
+ where
+  builder gp = addToStack (getX gp) (gpSlot gp)
+  getX = fst . gpCoord
 
 moveCrates :: Move -> Depot -> Depot
 moveCrates (quantity, from, to) m
@@ -108,15 +107,15 @@ moveCrates (quantity, from, to) m
   | otherwise = moveCrates (quantity - 1, from, to) (moveSingleCrate from to m)
 
 batchMoveCrates :: Move -> Depot -> Depot
-batchMoveCrates (quantity, from, to)
-  = uncurry (placeBatchOnStack to) . removeBatchFromStack quantity from 
+batchMoveCrates (quantity, from, to) =
+  uncurry (placeBatchOnStack to) . removeBatchFromStack quantity from
 
 removeBatchFromStack :: Int -> Int -> Depot -> ([Char], Depot)
-removeBatchFromStack quantity ident m = 
+removeBatchFromStack quantity ident m =
   (batch, M.update (Just . const remaining) ident m)
-  where
-    (batch, remaining) = splitCrates m
-    splitCrates = splitAt quantity . fromJust . M.lookup ident
+ where
+  (batch, remaining) = splitCrates m
+  splitCrates = splitAt quantity . fromJust . M.lookup ident
 
 placeBatchOnStack :: Int -> [Char] -> Depot -> Depot
 placeBatchOnStack ident crates = M.update (Just . (++) crates) ident
@@ -126,11 +125,11 @@ moveSingleCrate from to = uncurry (placeOnStack to) . removeFromStack from
 
 removeFromStack :: Int -> Depot -> (Char, Depot)
 removeFromStack ident m = fromJust payload
-  where
-    payload = do 
-      crates <- M.lookup ident m
-      (top, rest) <- uncons crates
-      return (top, M.update (Just . const rest) ident m)
+ where
+  payload = do
+    crates <- M.lookup ident m
+    (top, rest) <- uncons crates
+    return (top, M.update (Just . const rest) ident m)
 
 placeOnStack :: Int -> Char -> Depot -> Depot
 placeOnStack ident c = M.update (Just . (:) c) ident
@@ -138,38 +137,47 @@ placeOnStack ident c = M.update (Just . (:) c) ident
 topOfEachStack :: Depot -> Text
 topOfEachStack = T.pack . map (head . snd) . M.toList
 
-------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+solution :: (Move -> Depot -> Depot) -> Text -> Text
+solution mover input =
+  topOfEachStack $ foldr mover depot $ reverse moves
+ where
+  structureInput = fromRight ([], []) . runParser pInput ""
+  (points, moves) = structureInput input
+  depot = buildDepot points
 
 part1Solution :: Text -> Text
-part1Solution input = topOfEachStack $ foldr moveCrates depot $ reverse moves
-  where
-    structureInput = fromRight ([], []) . runParser pInput ""
-    (points, moves) = structureInput input
-    depot = buildDepot points
+part1Solution = solution moveCrates
+
+-- part1Solution input = topOfEachStack $ foldr moveCrates depot $ reverse moves
+--  where
+--   structureInput = fromRight ([], []) . runParser pInput ""
+--   (points, moves) = structureInput input
+--   depot = buildDepot points
 
 part2Solution :: Text -> Text
-part2Solution input = topOfEachStack $ foldr batchMoveCrates depot $ reverse moves
-  where
-    structureInput = fromRight ([], []) . runParser pInput ""
-    (points, moves) = structureInput input
-    depot = buildDepot points
+part2Solution = solution batchMoveCrates
 
-------------------------------------------------------------------------------------
+-- part2Solution input = topOfEachStack $ foldr batchMoveCrates depot $ reverse moves
+--  where
+--   structureInput = fromRight ([], []) . runParser pInput ""
+--   (points, moves) = structureInput input
+--   depot = buildDepot points
+
+--------------------------------------------------------------------------------
 
 puzzleInput :: Text
 puzzleInput =
   T.intercalate
     "\n"
-    [ "    [D]    ",
-      "[N] [C]    ",
-      "[Z] [M] [P]",
-      " 1   2   3 ",
-      "",
-      "move 1 from 2 to 1",
-      "move 3 from 1 to 3",
-      "move 2 from 2 to 1",
-      "move 1 from 1 to 2"
+    [ "    [D]    "
+    , "[N] [C]    "
+    , "[Z] [M] [P]"
+    , " 1   2   3 "
+    , ""
+    , "move 1 from 2 to 1"
+    , "move 3 from 1 to 3"
+    , "move 2 from 2 to 1"
+    , "move 1 from 1 to 2"
     ]
-
-example :: Text -> [GridPoint]
-example = fst . fromRight ([], []) . runParser pInput ""
