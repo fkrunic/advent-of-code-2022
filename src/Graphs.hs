ca8e7f3b7e0113f8eff4a1c116d8b7881329c76c
filipkrunic <fkrunic9@gmail.com>
2023-02-09 07:29:34 -0800

    setup for djikstra

--- src/Graphs.hs
+++ src/Graphs.hs
@@ -1,3 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Graphs where
 
 import Control.Monad.Trans.State.Strict
+import Data.Map (Map)
+import Data.Map qualified as M
+import Data.Set (Set)
+import Data.Set qualified as S
+import Data.Void (Void)
+
+data Distance
+  = Finite Word
+  | Infinite
+  deriving (Show, Eq, Ord)
+
+newtype Vertex a = Vertex a deriving (Show, Eq, Ord)
+type DistanceMap a = Map (Vertex a) Distance
+type VoidMap key = Map key Void
+
+data DijkstraSetup a = DijkstraSetup
+  { dist :: DistanceMap a
+  , prev :: DistanceMap a
+  , q :: VoidMap (Vertex a)
+  }
+  deriving (Show, Eq, Ord)
+
+setup :: Ord a => Vertex a -> [Vertex a] -> DijkstraSetup a
+setup source vertices = DijkstraSetup{..}
+ where
+  dist =
+    M.adjust (const (Finite 0)) source $
+      M.fromList $
+        map (,Infinite) vertices
+  prev = M.fromList $ map (,undefined) vertices
+  q = M.fromList $ map (,undefined) vertices
+
+  
