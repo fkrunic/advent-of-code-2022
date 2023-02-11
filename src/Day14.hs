7b5daa664b0cb7cd56597210cbeb7a18c8c79428
filipkrunic <fkrunic9@gmail.com>
2023-02-10 22:03:19 -0800

    attempt to define grid and draw elements

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,12 +1,16 @@
 module Day14 where
 
+import Data.Map (Map)
+import Data.Map qualified as M
+import Data.Text (Text)
+
 import Grids
 import Parsing
 
 import Text.Megaparsec
 
 type DrawPath = [Coordinate]
-data Element = Sand | Rock | Abyss deriving (Show, Eq) 
+data Element = Sand | Rock | Air | Abyss | Source deriving (Show, Eq)
 
 --------------------------------------------------------------------------------
 
@@ -42,10 +46,38 @@ pointsAlong c1 c2
 
 chainPath :: [Coordinate] -> Maybe [Coordinate]
 chainPath = foldr builder (Just [])
-  where
-    builder _ Nothing = Nothing
-    builder coord (Just []) = Just [coord]
-    builder coord (Just (x:rest)) = (++ rest) <$> pointsAlong coord x
+ where
+  builder _ Nothing = Nothing
+  builder coord (Just []) = Just [coord]
+  builder coord (Just (x : rest)) = (++ rest) <$> pointsAlong coord x
 
-defineGrid :: [DrawPath] -> Grid Element
-defineGrid rocks = undefined
+bulkAdjust :: Ord k => [k] -> (v -> v) -> Map k v -> Map k v
+bulkAdjust keys update m = foldr (M.adjust update) m keys
+
+defineGrid :: [DrawPath] -> Maybe (Grid Element)
+defineGrid dps = do
+  rocks <- concat <$> mapM chainPath dps
+  let combined = source : rocks
+      xMin = minimum $ map (unpackX . fst) combined
+      xMax = maximum $ map (unpackX . fst) combined
+      yMin = minimum $ map (unpackY . snd) combined
+      yMax = maximum $ map (unpackY . snd) combined
+      gridXMin = xMin - 1
+      gridXMax = xMax + 1
+      gridYMax = yMax + 1
+      abyssRow = [point x gridYMax | x <- [gridXMin .. gridXMax]]
+      gridCoords = [point x y | x <- [xMin .. xMax], y <- [yMin .. yMax]]
+      canvas = M.fromList $ map (,Air) gridCoords
+      grid =
+        bulkAdjust abyssRow (const Abyss) $
+          bulkAdjust rocks (const Rock) canvas
+  return $ M.adjust (const Source) source grid
+ where
+  source = point 500 0
+
+drawElement :: Element -> Text
+drawElement Sand = "o"
+drawElement Rock = "#"
+drawElement Air = "."
+drawElement Abyss = "*"
+drawElement Source = "+"
