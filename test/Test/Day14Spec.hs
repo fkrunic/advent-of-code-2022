2d34029b7f47f97aaad63a253a0e9ab533b4d814
filipkrunic <fkrunic9@gmail.com>
2023-02-10 21:39:32 -0800

    using point helper function

--- test/Test/Day14Spec.hs
+++ test/Test/Day14Spec.hs
@@ -10,43 +10,44 @@ spec =
   describe "Day 14 Tests" $ do
     it "Parsing" $ do
       let expected =
-            [ (XCoordinate 498, YCoordinate 4)
-            , (XCoordinate 498, YCoordinate 6)
-            , (XCoordinate 496, YCoordinate 6)
+            [ point 498 4
+            , point 498 6
+            , point 496 6
             ]
           actual = runParser pDrawPath "" "498,4 -> 498,6 -> 496,6"
       actual `shouldBe` Right expected
 
     describe "Points Along Tests" $ do
       it "Points along (1,1) and (1,3)" $ do
-        let start = (XCoordinate 1, YCoordinate 1)
-            middle = (XCoordinate 1, YCoordinate 2)
-            end = (XCoordinate 1, YCoordinate 3)
+        let start = point 1 1
+            middle = point 1 2
+            end = point 1 3
         pointsAlong start end `shouldBe` Just [start, middle, end]
         pointsAlong end start `shouldBe` Just [end, middle, start]
 
       it "Points along (4,1) and (1,1)" $ do
-        let start = (XCoordinate 4, YCoordinate 1)
-            m1 = (XCoordinate 3, YCoordinate 1)
-            m2 = (XCoordinate 2, YCoordinate 1)
-            end = (XCoordinate 1, YCoordinate 1)
+        let start = point 4 1
+            m1 = point 3 1
+            m2 = point 2 1
+            end = point 1 1
         pointsAlong start end `shouldBe` Just [start, m1, m2, end]
         pointsAlong end start `shouldBe` Just [end, m2, m1, start]
 
       it "No points along (1,1) and (3,3)" $ do
-        let start = (XCoordinate 1, YCoordinate 1)
-            end = (XCoordinate 3, YCoordinate 3)
+        let start = point 1 1
+            end = point 3 3
         pointsAlong start end `shouldBe` Nothing
         pointsAlong end start `shouldBe` Nothing
 
       it "Chaining points (1,1) -> (3,1) -> (3,3)" $ do
-        let p1 = point 1 1
-            p2 = point 2 1
-            p3 = point 3 1
-            p4 = point 3 2
-            p5 = point 3 3
-            expected = [p1, p2, p3, p4, p5]
-        chainPath [p1, p3, p5] `shouldBe` Just expected
+        chainPath [point 1 1, point 3 1, point 3 3]
+          `shouldBe` Just
+            [ point 1 1
+            , point 2 1
+            , point 3 1
+            , point 3 2
+            , point 3 3
+            ]
 
       it "Chaining points (5,1) -> (1,1) -> (1,5)" $ do
         chainPath [point 5 1, point 1 1, point 1 5]
