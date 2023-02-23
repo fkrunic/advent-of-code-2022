99e2c0a36a0b6b4010e0263958c13ea3eb751517
filipkrunic <fkrunic9@gmail.com>
2023-02-22 21:09:14 -0800

    instantiating infinite functionality

--- src/Infinites.hs
+++ src/Infinites.hs
@@ -1,8 +1,15 @@
 module Infinites where
 
 import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty qualified as NE
 
 newtype Infinite a = Infinite (NonEmpty a)
 
+makeInf :: NonEmpty a -> Infinite a
+makeInf = Infinite . NE.cycle
+
 getSplit :: Infinite a -> (a, Infinite a)
-getSplit = undefined
+getSplit (Infinite cyc) =
+  case NE.uncons cyc of
+    (_, Nothing) -> error "Infinite instance does not have tail elements."
+    (front, Just rest) -> (front, Infinite rest)
