504c89d0966da8b73264d69128bb44540634efd7
filipkrunic <fkrunic9@gmail.com>
2023-02-22 18:51:51 -0800

    more scaffolding

--- src/Day17.hs
+++ src/Day17.hs
@@ -7,52 +7,107 @@ import Data.List.NonEmpty qualified as NE
 
 import Grids
 
-data RockType = HLine | Plus | LShape | VLine | Square
-  deriving (Show, Eq, Ord, Enum)
+-- data RockType = HLine | Plus | LShape | VLine | Square
+--   deriving (Show, Eq, Ord, Enum)
 
 data WindDirection = East | West deriving (Show, Eq, Ord)
 data Action = Blow WindDirection | Fall deriving (Show, Eq, Ord)
 
-newtype CaveBoundaries = CaveBoundaries Boundaries deriving (Show, Eq)
+-- newtype CaveBoundaries = CaveBoundaries Boundaries deriving (Show, Eq)
 data Cell = Empty | Surface deriving (Show, Eq)
 type Cave = Grid Cell
 
 newtype RockPosition = RockPosition (NonEmpty Coordinate) deriving (Show, Eq)
 newtype RockBottom = RockBottom (NonEmpty Coordinate) deriving (Show, Eq)
-type Rock = (RockType, RockPosition)
+-- type Rock = (RockType, RockPosition)
 
-newtype RocksDropped = RocksDropped Int deriving (Show, Eq)
-newtype TowerHeight = TowerHeight Int deriving (Show, Eq)
+-- newtype RocksDropped = RocksDropped Int deriving (Show, Eq, Num)
+-- newtype TowerHeight = TowerHeight Int deriving (Show, Eq)
 
-data TowerProcess = TowerProcess
-  { cave :: Cave
-  , rocksDropped :: Int
-  , towerHeight :: Int
-  }
-  deriving (Show, Eq)
+-- data TowerProcess = TowerProcess
+--   { cave :: Cave
+--   , rocksDropped :: Int
+--   , towerHeight :: Int
+--   }
+--   deriving (Show, Eq)
+
+data TowerProcess = FallingRock RockPosition | SettledRock Cave
+
+newtype Infinite a = Infinite (NonEmpty a)
+
+getNext :: Infinite a -> a
+getNext = undefined
+
+getRest :: Infinite a -> Infinite a 
+getRest = undefined
 
 --------------------------------------------------------------------------------
 
-blow :: CaveBoundaries -> WindDirection -> RockPosition -> Maybe RockPosition
-blow = undefined
+canBlow :: Cave -> WindDirection -> RockPosition -> Bool
+canBlow = undefined
 
-fall :: CaveBoundaries -> RockPosition -> Maybe RockPosition
-fall = undefined
+canDrop :: Cave -> RockBottom -> Bool
+canDrop = undefined
 
-bottomSurface :: Rock -> RockBottom
-bottomSurface = undefined
+blowRock :: Cave -> WindDirection -> RockPosition -> RockPosition
+blowRock = undefined
 
-canFall :: Cave -> RockBottom -> Bool
-canFall = undefined
+dropRock :: RockPosition -> RockPosition
+dropRock = undefined
 
-solidifyRock :: Cave -> Rock -> Cave
-solidifyRock = undefined
+settleRock :: Cave -> RockPosition -> Cave
+settleRock = undefined
 
-startPosition :: Cave -> RockType -> Rock
-startPosition = undefined
+getBottom :: RockPosition -> RockBottom
+getBottom = undefined
 
-rockTypes :: [RockType]
-rockTypes = cycle [HLine .. Square]
+--------------------------------------------------------------------------------
+
+dropProcess :: Cave -> RockPosition -> TowerProcess
+dropProcess cave rp = 
+  if canDrop cave bottom 
+    then FallingRock (dropRock rp)
+    else SettledRock (settleRock cave rp)
+  where
+    bottom = getBottom rp
+
+blowProcess :: Cave -> RockPosition -> WindDirection -> TowerProcess
+blowProcess cave rp direction = 
+  if canBlow cave direction rp
+    then FallingRock (blowRock cave direction rp)
+    else FallingRock rp
+
+rockProcess :: Cave -> RockPosition -> Infinite WindDirection -> Cave
+rockProcess cave rp wdrs = 
+  case blowProcess cave rp (getNext wdrs) of
+    FallingRock rp' -> 
+      case dropProcess cave rp' of 
+        FallingRock rp'' -> rockProcess cave rp'' (getRest wdrs)
+        SettledRock cave' -> cave'
+
+
+-- blow :: Cave -> WindDirection -> RockPosition -> Maybe RockPosition
+-- blow = undefined
+
+-- fall :: Cave -> RockPosition -> Maybe RockPosition
+-- fall = undefined
+
+-- bottomSurface :: Rock -> RockBottom
+-- bottomSurface = undefined
+
+-- canFall :: Cave -> RockBottom -> Bool
+-- canFall = undefined
+
+-- solidifyRock :: Cave -> Rock -> Cave
+-- solidifyRock = undefined
+
+-- startPosition :: Cave -> RockType -> Rock
+-- startPosition = undefined
+
+-- rockTypes :: [RockType]
+-- rockTypes = cycle [HLine .. Square]
+
+-- dropProcess :: Cave -> Rock -> 
 
 --------------------------------------------------------------------------------
 
