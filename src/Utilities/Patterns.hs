d7b5bce83ddab5e36d656fe7e0c8b487760747a0
filipkrunic <fkrunic9@gmail.com>
2023-02-24 11:51:03 -0800

    pattern implementation

--- src/Utilities/Patterns.hs
+++ src/Utilities/Patterns.hs
@@ -1,7 +1,7 @@
 module Utilities.Patterns where
 
 import Data.IntSet qualified as IS
-import Data.List (groupBy)
+import Data.List (group, groupBy)
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty qualified as NE
 import Math.NumberTheory.ArithmeticFunctions
@@ -10,15 +10,34 @@ newtype Divisor = Divisor Int deriving (Show, Eq)
 newtype Offset = Offset Int deriving (Show, Eq)
 newtype PatternLength = PatternLength Int deriving (Show, Eq)
 
-findPattern :: Eq a => [a] -> (Offset, Maybe PatternLength)
-findPattern = undefined
+findPattern :: Eq a => Int -> [a] -> (Offset, Maybe PatternLength)
+findPattern k xs = foldr checker Nothing [0 .. length xs]
+ where
+  xsLength = length xs
+  checker i acc =
+    let (front, back) = splitAt i xs
+        chopped = chop i back
+     in if length (group chopped) == 1
+          then (Offset, Just $ PatternLength xsLength)
+          else (Offset, Nothing)
 
-getDivisors :: Int -> NonEmpty Divisor
-getDivisors = NE.map Divisor . NE.fromList . IS.toList . divisorsSmall
+hasPattern :: Eq a => Offset -> PatternLength -> [a] -> Bool
+hasPattern (Offset offset) (PatternLength pattern) xs =
+  length (group chopped) == 1
+ where
+  (front, back) = splitAt offset xs
+  chopped = chop pattern xs
 
-equalSizeGroups :: Int -> [a] -> [[a]]
-equalSizeGroups k =
+getDivisors :: Int -> [Divisor]
+getDivisors =
+  NE.toList
+    . NE.map Divisor
+    . NE.fromList
+    . IS.toList
+    . divisorsSmall
+
+chop :: Int -> [a] -> [[a]]
+chop k =
   map (map snd)
     . groupBy (\t1 t2 -> fst t1 == fst t2)
-    . zipWith (\i x -> (i `div` k, x)) [1 :: Int ..]
-
+    . zipWith (\i x -> (i `div` k, x)) [0 :: Int ..]
